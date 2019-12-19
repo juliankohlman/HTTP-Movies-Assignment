4465381956005e8093c2366688a5392b911e2777
Julian Kohlman <juliankohlman@gmail.com>
2019-12-19 11:33:39 -0500

    wip mvp

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -31,6 +31,23 @@ export default class Movie extends React.Component {
     addToSavedList(this.state.movie);
   };
 
+  updateMovie = () => {
+    // update movie logic here
+    console.log(this.state.movie.id);
+    let updateId = this.state.movie.id;
+    this.props.history.push(`/update-movie/${updateId}`);
+  };
+
+  deleteMovie = () => {
+    axios
+      .delete(`http://localhost:5000/api/movies/${this.state.movie.id}`)
+      .then(res => {
+        console.log(res);
+        this.props.history.push(`/`);
+      })
+      .catch(err => console.error(err));
+  };
+
   render() {
     if (!this.state.movie) {
       return <div>Loading movie information...</div>;
@@ -42,6 +59,10 @@ export default class Movie extends React.Component {
         <div className="save-button" onClick={this.saveMovie}>
           Save
         </div>
+        <div className="update-button" onClick={this.updateMovie}>
+          Update
+        </div>
+        <button onClick={this.deleteMovie}>Delete</button>
       </div>
     );
   }
